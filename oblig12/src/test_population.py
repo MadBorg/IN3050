1730477a21b6afda2ac37c7599f9f943130e5af7
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2020-03-09 21:34:49 +0100

    made progress, see main

--- oblig12/src/test_population.py
+++ oblig12/src/test_population.py
@@ -5,17 +5,22 @@ import data
 
 df = data.data(main.path_to_datafile)
 
-N = 100
+population_size = 100
 subset_size = 6
+parent_selection_portion = 0.5
+number_of_offsprings = 200
 cities_data = data.data()
 cities_data.read_csv(main.path_to_datafile)
+subset = cities_data.get_subset(subset_size)
 representation = cities_data.get_representation(N=subset_size, start=0)
 
 pop = population.Population(
     genotype.Genotype,
     representation,
     main.fit,
-    N
+    population_size,
+    parent_selection_portion,
+    number_of_offsprings
 )
 
 def test_population_create_population():
@@ -27,20 +32,19 @@ def test_population_create_population():
                 counter +=1
     assert counter != len(pop._population), f"pop: {pop()}"
 
-# def test_population_recombination():
-#     number_of_offsprings = 200
-#     couples = [(i, i+1) for i in range(0,100,2)] # TODO : Implement parent selection
-#     offsprings = pop.recombination(
-#         couples,
-#         number_of_offsprings
-#     )
+def test_population_recombination():
+    couples = [(i, i+1) for i in range(0,100,2)] # TODO : Implement parent selection
 
-#     assert len(offsprings) == number_of_offsprings, "len offsprings is not the same as number of offsprings" +\
-#         f"len(offsprings): {len(offsprings)}, number of offsprings: {number_of_offsprings}!"
-#     for offsping in offsprings:
-#         assert len(offsping) == subset_size, "len offsping is not equal subset size"
+    pop.evaluate_population(subset)
+    pop.parent_selection()
+    offsprings = pop.recombination()
+
+    assert len(offsprings) == number_of_offsprings, "len offsprings is not the same as number of offsprings" +\
+        f"len(offsprings): {len(offsprings)}, number of offsprings: {number_of_offsprings}!"
+    for offsping in offsprings:
+        assert len(offsping) == subset_size, "len offsping is not equal subset size"
 
 
 if __name__ == "__main__":
     test_population_create_population()
-    # test_population_recombination()
+    test_population_recombination()
